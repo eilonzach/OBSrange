14706f3542a29dd5acb3b16ec7f0c2b578ed766b
jbrussell <jbrussell93@gmail.com>
2019-01-24 11:21:22 -0500

    added missing field survt to cardinal survey output

--- synthetics/synth_survey_cardinal.m
+++ synthetics/synth_survey_cardinal.m
@@ -15,7 +15,7 @@ gps_offset_str = 'fr10';
 
 niter = 1e4; %1;%1e4; % if niter>1, will not make plots or save output file in SIO format
 
-ifsave = false;
+ifsave = true;
 
 %% system/default parameters
 nm2km = 1.852;
@@ -85,7 +85,7 @@ if niter > 1
                   'TG_dforward',dforward,'TG_dstarboard',dstarboard,...
                   'obs_loc_xyz',[],'obs_loc_laloz',[],...
                   'TAT',[],'Vp_water',[],...
-                  'Nobs',[],'survlats',[],'survlons',[],'survts',[],'tot_dt',[],...
+                  'Nobs',[],'survlats',[],'survlons',[],'survt',[],'tot_dt',[],...
                   'corr_dt',[],'v_surv_true',[]);
 end
                              
@@ -164,6 +164,7 @@ survt = now + [1:Nping];
 data(jj).Nobs = length(survlon);
 data(jj).survlats = survlat;
 data(jj).survlons = survlon;
+data(jj).survt = survt;
 data(jj).tot_dt = tot_dt;
 data(jj).corr_dt = corr_dt;
 data(jj).v_surv_true = surv_vel_true;
