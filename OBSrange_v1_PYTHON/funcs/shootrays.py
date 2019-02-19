36729cc681b4ab355f9b88000d5e6ccb5e28362e
s-g-mo <stephenmosher@gmail.com>
2019-02-19 07:05:39 -0500

    finished shootrays.py

--- OBSrange_v1_PYTHON/funcs/shootrays.py
+++ OBSrange_v1_PYTHON/funcs/shootrays.py
@@ -8,6 +8,7 @@ Translated to Python and further modified by S. Mosher 01/2019
 '''
 # Import modules and functions
 import numpy as np
+from scipy.interpolate import interp1d
 
 from IPython.core.debugger import Tracer
 
@@ -31,7 +32,6 @@ def linterp(X, Y, XI):
 
   ilay = find_ilay(XI, X)
   
-  Tracer()()
   # Linearly interpolate.
   YI = (XI - X[ilay-1]) * (Y[ilay] - Y[ilay - 1]) / (X[ilay] - X[ilay - 1]) + Y[ilay - 1]
 
@@ -45,16 +45,13 @@ def linterp(X, Y, XI):
 def shootrays(p, v_profile, zmax, dr=0.001, vdz=0.001):
 
   # Anonymous functions
-  eta = lambda u, p: np.sqrt(u**2 + p**2)
+  eta = lambda u, p: np.sqrt(u**2 - p**2)
   gradv_dist = lambda b, u1, u2, p: (eta(u1,p)/(b*u1*p)) - (eta(u2,p)/(b*u2*p))
   constv_dist = lambda u, dz, p: (p * dz) / eta(u, p)
 
-
-  Tracer()()
   zz1 = np.arange(0, zmax, vdz)
-  zz2 = np.arange(vdz, zmax+vdz, vdz)
+  zz2 = np.arange(vdz, zmax + vdz, vdz)
   
-
   v1  = linterp(v_profile[0], v_profile[1], zz1)
   v2  = linterp(v_profile[0], v_profile[1], zz2)
 
@@ -70,30 +67,33 @@ def shootrays(p, v_profile, zmax, dr=0.001, vdz=0.001):
   
   X = np.zeros(len(v1))
   X[const_indx == True] = constv_dist( u1[const_indx == True], dz[const_indx == True], p )
-  X[const_indx != True] = gradv_dist(b[const_indx != True], u1[const_indx != True],u2[const_indx != True],p)
-  X = np.hstack([0, X])
+  X[const_indx != True] = gradv_dist(b[const_indx != True], u1[const_indx != True], u2[const_indx != True], p)
   X[np.imag(X) > 0] = np.nan
   X = X[X != np.isnan(X)]
+  X = np.hstack([0, X])
   Xd = np.cumsum(X)
-  rayz = np.hstack([zz1[1], zz2]) 
-  #rayz = rayz(1:length(X))
-  #dz = dz(1:length(X)-1)
-  
-  # calc Dr
-  Dr1 = np.sqrt(X**2 + dz**2)
+  rayz = np.hstack([zz1[0], zz2]) 
+  rayz = rayz[0:len(X)]
+  dz = dz[0:len(X) - 1]
+ 
+  # Calculate Dr
+  Dr1 = np.sqrt(X[1:]**2 + dz**2)
   assert(np.imag(Dr1).any() == False)
   Dr1 = np.hstack([0, Dr1])
   Dr2 = np.cumsum(Dr1)
-  #Dr = np.unique( np.hstack( [np.linspace(0, max(Dr2) + dr, dr), max(Dr2)])).T
-  #rx = interp1(Dr2,Xd,Dr)
-  #rz = interp1(Dr2,rayz,Dr)
-  #
-  #% calculate travel time (JBR)
-  #dDr = Dr(2:length(Dr))-Dr(1:length(Dr)-1)
-  #dDr = [0; dDr]
-  #v = [v1(1; v2)]
-  #rv = interp1(Dr2,v,Dr)
-  #dt = dDr./rv
-  #rt = cumsum(dt)
+  Dr = np.unique( np.hstack([np.arange(0, max(Dr2), dr), max(Dr2)]))
+  fx = interp1d(Dr2, Xd)
+  fz = interp1d(Dr2, rayz)
+  rx = fx(Dr)
+  rz = fz(Dr)
 
-  #return 
+  # Calculate travel-time (JBR)
+  dDr = Dr[1:len(Dr)] - Dr[0:len(Dr)-1]
+  dDr = np.hstack([0, dDr])
+  v = np.hstack([v1[0], v2])
+  fv = interp1d(Dr2, v)
+  rv = fv(Dr)
+  dt = dDr / rv
+  rt = np.cumsum(dt)
+
+  return rx, rz, Dr, rt, rv
